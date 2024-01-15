9829cc4f8b6ef2f64bf8baa757293f81c0a79e3e
AhmedSamehh <a7medse7a10@gmail.com>
2024-01-15 18:21:28 +0200

    adding auth controller

--- src/main/java/com/seha/springauth/repository/UserRepository.java
+++ src/main/java/com/seha/springauth/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.seha.springauth.repository;
 
 import com.seha.springauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Boolean existsByUsername(String username);
